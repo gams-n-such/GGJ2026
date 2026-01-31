ebcaa1eb6bac3b73cf293ca444270a4988ad23b3
Dav1dovv <davitmkrtchyan206@gmail.com>
2026-01-31 18:06:23 +0300

    Plant and infection

--- Assets/Plants/Plant.gd
+++ Assets/Plants/Plant.gd
@@ -7,10 +7,19 @@ extends Node3D
 @export var half_infection : Texture2D
 @export var full_infection : Texture2D
 
+var phase
+
 func _on_infection_infection_state_changed() -> void:
 	if infection.progress == 0:
+		phase = 0
 		sprite.material_override.albedo_texture = none_infection
 	elif infection.progress == 1:
+		phase = 1
 		sprite.material_override.albedo_texture = half_infection
 	elif infection.progress == 2:
+		phase = 2
 		sprite.material_override.albedo_texture = full_infection
+
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	Utils.deal_damage(body,1)
