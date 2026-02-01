fa8b88c4c803b91444dcf45fbc1c531a1de462b6
Dav1dovv <davitmkrtchyan206@gmail.com>
2026-02-01 10:55:08 +0300

    Mask Slot and something

--- Misc/MaskSlot/mask_slot.gd
+++ Misc/MaskSlot/mask_slot.gd
@@ -7,18 +7,23 @@ enum mask_type {blue, red,white}
 
 @export var mask : mask_type = mask_type.white
 
-var mask_icons := [preload("uid://347lrplpcwiw"), 
-preload("uid://dxeu5ljjtxg41"), 
-preload("uid://fffulgw8jjbh")
+@onready var mask_hots := [$HBoxContainer/Hot/BluemaskIcon,
+$HBoxContainer/Hot2/RedmaskIcon,
+$HBoxContainer/Hot3/GreenmaskIcon
 ]
+const MASK_BLUE = preload("uid://347lrplpcwiw")
+const MASK_RED = preload("uid://dxeu5ljjtxg41")
+const MASK_WHITE = preload("uid://fffulgw8jjbh")
 
+var mask_icons = [MASK_BLUE,MASK_RED,MASK_WHITE]
 func _ready() -> void:
 	icon.texture = mask_icons[mask]
-
+	mask_hots[mask].visible = true
 
 func Activate(change_to_mask : mask_type):
 	print("activating mask with index: " + str(change_to_mask))
-	for child in $Hot.get_children():
-		child.visible = false
-	if change_to_mask == mask:
-		$Hot.get_child(change_to_mask).visible = true
+	for i in mask_hots.size():
+		mask_hots[i].visible= false
+	
+	mask_hots[change_to_mask].visible = true
+	icon.texture = mask_icons[change_to_mask]
