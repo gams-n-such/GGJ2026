c515df7fbc0e1aa06ea76ce06f5f7342a9669164
Dav1dovv <davitmkrtchyan206@gmail.com>
2026-02-01 04:59:24 +0300

    ltl

--- Core/Equipment/equipment.gd
+++ Core/Equipment/equipment.gd
@@ -2,6 +2,7 @@ class_name Equipment
 extends Node
 
 @export var equip_input_action : StringName
+@export var activate_mask_index : MaskSlot.mask_type
 @export var resource : EquipmentResource
 
 signal equip_requested(equipment : Equipment)
@@ -12,12 +13,14 @@ var transitioning := false
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed(equip_input_action):
 		equip_requested.emit(self)
+		
 
 func equip() -> void:
 	transitioning = true
 	await get_tree().create_timer(1.0).timeout
 	transitioning = false
 	active = true
+	get_tree().call_group("MaskSlot","Activate",activate_mask_index)
 	print("Equipped " + str(self))
 
 func unequip() -> void:
---
+++ Misc/MaskSlot/mask_slot.gd
@@ -0,0 +1,22 @@
+class_name MaskSlot
+extends Control
+
+enum mask_type {blue, red,white}
+
+@onready var icon: TextureRect = $icon
+
+@export var mask : mask_type = mask_type.white
+
+var mask_icons := [preload("uid://347lrplpcwiw"), 
+preload("uid://dxeu5ljjtxg41"), 
+preload("uid://fffulgw8jjbh")
+]
+
+func _ready() -> void:
+	icon.texture = mask_icons[mask]
+
+
+func Activate(change_to_mask : mask_type):
+	for i in $Hot.get_child_count():
+		$Hot.get_child(i).visible = false
+	$Hot.get_child(mask).visible = true
