a3b65ed3ef06ec391a7b472f7f26901b2930e92f
Denis Maximenko <manlok71@gmail.com>
2026-02-01 14:56:06 +0400

    Fixes

--- Misc/GUI/capsule_count.gd
+++ Misc/GUI/capsule_count.gd
@@ -1,5 +1,5 @@
 extends AutoSizeLabel
 
 func _process(delta: float) -> void:
-	text = "collected capsules: " + str(Game.collected_parts + Game.delivered_vields) + "/" + str(Game.total_parts)
+	text = "collected capsules: " + str(Game.collected_vials + Game.delivered_vials) + "/" + str(Game.total_vials)
 	pass
--- Misc/GUI/parts_count.gd
+++ Misc/GUI/parts_count.gd
@@ -1,5 +1,5 @@
 extends AutoSizeLabel
 
 func _process(delta: float) -> void:
-	text = "parts finded : "  + str(Game.collected_vials + Game.delivered_vields) + "/" + str(Game.total_vials)
+	text = "parts finded : "  + str(Game.collected_vials + Game.delivered_vials) + "/" + str(Game.total_vials)
 	pass
